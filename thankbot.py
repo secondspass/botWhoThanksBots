e5f341d25ebf6806786145e5fe39658d836960a8
Subil <subil1407@gmail.com>
2016-12-19 13:06:06 +0530

    added exception handling to the main loop

--- thankbot.py
+++ thankbot.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!usr/bin/python3
 
 """
 A Reddit bot that thanks other bots
@@ -10,7 +10,9 @@ make a comment more than once every 10 minutes.
 import praw
 import time
 
-botlist = ['RemindMeBot', 'AutoModerator', 'TotesMessenger', 'samacharbot2']
+botlist = ['RemindMeBot', 'AutoModerator', 'TotesMessenger', 'samacharbot2'
+           'TweetsInCommentsBot'
+           ]
 replied_comments = {}
 comment_string = """
 Thank you {}!
@@ -24,7 +26,7 @@ Thank you {}!
 
 def post_reply(botname, comment):
     try:
-        if replied_comments[comment.id]: 
+        if replied_comments[comment.id]:
             return
     except KeyError:
         try:
@@ -32,23 +34,26 @@ def post_reply(botname, comment):
         except Exception as e:
             print("Moving to the next bot because exception occured:", str(e))
             return
-            
+
         replied_comments[comment.id] = True
         print("Thanked {}. id: {}".format(botname, comment.id))
 
 
 def thank_the_bots():
     for botname in botlist:
-        reddit = praw.Reddit('thankerbot')
-        bot_object = reddit.redditor(botname)
-        # bot_new_submissions includes posts and comments
-        bot_new_submissions = bot_object.new(limit=1)
-        for submission in bot_new_submissions:
-            print("retrieving next comment")
-            if isinstance(submission, praw.models.reddit.comment.Comment):
-                post_reply(botname, submission)
-                print("waiting 10 minutes because reddit doesn't want spam")
-                time.sleep(600)
+        try:
+            reddit = praw.Reddit('thankerbot')
+            bot_object = reddit.redditor(botname)
+            # bot_new_submissions includes posts and comments
+            bot_new_submissions = bot_object.new(limit=1)
+            for submission in bot_new_submissions:
+                print("retrieving next comment")
+                if isinstance(submission, praw.models.reddit.comment.Comment):
+                    post_reply(botname, submission)
+                    print("waiting 10 minutes because reddit doesn't want spam")
+                    time.sleep(600)
+        except Exception as e:
+            print("some exception occured so moving on:", e)
 
 
 if __name__ == '__main__':
